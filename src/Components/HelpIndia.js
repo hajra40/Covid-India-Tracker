155092053162ed8530237a09e5a571b56e26a417
Hajra <hajra.mshahid24@gmail.com>
2021-04-30 14:41:17 +0530

    v.3.8

--- src/Components/HelpIndia.js
+++ src/Components/HelpIndia.js
@@ -8,9 +8,9 @@ const HelpIndia = () => {
             <img src={help} alt="helpIndia" className="img"/>
             <div className="help__container">
                 <h1>Let's Help India Breathe</h1>
-                <Typography color="textPrimary" style={{margin: '1rem 0rem'}}>
-                    Everyday thousand of people lose their lives <br />due to oxygen shortage , no ventilators & beds available in hospitals. <br />
-                    Hospitals are filled with covid patients,<br /> people are dying on the floor , roads , coridors.<br /> India needs you. 
+                <Typography color="textPrimary" style={{margin: '1rem 0rem' , textAlign: 'left'}}>
+                    Everyday thousand of people are losing their lives <br />due to oxygen shortage , no ventilators & beds available . <br />
+                    Hospitals are completely filled with covid patients,<br /> people are dying on floor , roads , coridors.<br /> India needs you. 
                 </Typography>
                 <h4><a href="https://milaap.org/#:~:text=Free%20Crowdfunding%20for%20India&text=%231%20Fundraising%20website%20in%20India,Milaap">Donate</a></h4>
                 <h4><a href="https://www.mohfw.gov.in/covid_vaccination/vaccination/index.html">Register for vaccination</a></h4>
